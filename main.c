eed6555047da138a9c523ec562f4f3ebb0533317
tonyedavids <davidstonye@gmail.com>
2023-12-27 10:49:42 -0500

    cmms

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main() {
+   // variable types
   int a = 3;
   float b = 4.5;
   double c = 5.25;
@@ -9,5 +10,20 @@ int main() {
     sum = a + b + c;
 
   printf("The sum of a, b, and c is exactly %f.", sum);
+
+  // arrays
+  int grades[3]; // need to state the size of array first
+  int average;   // need to assign an the average memeory to be used later
+
+  grades[0] = 80;
+  grades[1] = 85;
+  grades[2] = 90;   // assigned every part of the list 
+
+  average = (grades[0] + grades[1] + grades[2]) / 3;
+  printf("The average of the 3 grades is: %d", average);
+
   return 0;
+
+  // multi dimensional arrays
+  
 }
